ae043a4f3f6ef0843cf12db5398a3dce1fb29470
ladal1 <ladislav.louka@ackee.cz>
2023-02-22 13:26:38 +0100

    Prisma start

---
+++ src/Packages/prismaORM/index.ts
@@ -0,0 +1,20 @@
+// import IORMPackage from 'BenchmarkUtils/interfaces/PackageUtils'
+
+// const ClientMap = new Map()
+
+// const initialize = async () => {
+// }
+
+// const destroy = async () => {
+// }
+
+// const PgTypedPackage: IORMPackage = {
+//   name: 'PgTyped',
+//   initialize,
+//   destroy,
+//   implementations: {
+//     EntityTraversal,
+//   },
+// }
+
+// export { PgTypedPackage }
