ea5bc460aae2863ac4075cc5229f8559606f64f5
ladal1 <ladislav.louka@ackee.cz>
2023-03-09 17:29:08 +0100

    Zapatos EntityTraversal

--- src/Packages/zapatos/Benchmarks/EntityTraversal.ts
+++ src/Packages/zapatos/Benchmarks/EntityTraversal.ts
@@ -1,27 +1,68 @@
-import { Skipped } from 'BenchmarkUtils/BenchmarkRunner'
 import * as db from 'zapatos/db'
+import type * as schema from 'zapatos/schema'
 import { pgPool } from '..'
 import { EntityTraversalTest } from 'Benchmarks/EntityTraversal'
 
 const EntityTraversal: EntityTraversalTest = {
   getCatColor: async id => {
-    const joinedCat = await db
-      .select('cat', db.all, {
+    return db
+      .selectOne('cat', db.all, {
         lateral: {
-          cat_color: db.selectExactlyOne('cat_color', {
-            id: db.parent('cat_color_id'),
-          }),
+          cat_color: db.selectExactlyOne(
+            'cat_color',
+            {
+              id: db.parent('cat_color_id'),
+            },
+            {
+              lateral: {
+                color_hex: db.selectExactlyOne('color_hex', {
+                  id: db.parent('id'),
+                }),
+              },
+            }
+          ),
         },
       })
       .run(pgPool)
-    console.log(joinedCat)
-    throw new Skipped()
+      .then(cat => cat?.cat_color.color_hex.hex_code ?? '')
   },
   countCatsByColor: async hexColor => {
-    throw new Skipped()
+    type selectedTables =
+      | schema.cat.SQL
+      | schema.cat_color.SQL
+      | schema.color_hex.SQL
+    return db.sql<selectedTables, Array<{ count: string }>>`SELECT
+    COUNT(*)
+  FROM
+      ${'cat'}
+      JOIN ${'cat_color'} ON cat_color.id = ${'cat'}.${'cat_color_id'}
+      JOIN ${'color_hex'} ON ${'color_hex'}.${'id'} = ${'cat_color'}.${'id'}
+  WHERE
+      ${'color_hex'}.${'hex_code'} = ${db.param(hexColor)};`
+      .run(pgPool)
+      .then(rows => Number(rows[0].count))
   },
   getToysAvailableToCat: async id => {
-    throw new Skipped()
+    const toyNameColumn = ['toy_name'] as const
+    type toyName = schema.toy.OnlyCols<typeof toyNameColumn>
+    type selectedTables =
+      | schema.toy.SQL
+      | schema.toy_house.SQL
+      | schema.house.SQL
+      | schema.house_cat.SQL
+
+    return db.sql<selectedTables, toyName[]>`
+    SELECT
+      ${'toy'}.${'toy_name'}
+    FROM
+      ${'toy'}
+      JOIN ${'toy_house'} ON ${'toy_house'}.${'toy_id'} = ${'toy'}.${'id'}
+      JOIN ${'house'} ON house.id = ${'toy_house'}.${'house_id'}
+      JOIN ${'house_cat'} ON ${'house_cat'}.${'house_id'} = ${'house'}.${'id'}
+    WHERE
+      house_cat.cat_id = ${db.param(id)};`
+      .run(pgPool)
+      .then(toys => toys.map(toy => toy.toy_name))
   },
 }
 
